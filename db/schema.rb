58ca8022cdd6c0a24700056eb16082a5561e239b
Joost Lubach <joost@yoazt.com>
2015-02-09 09:39:35 +0100

    Prepare for Heroku deploy

--- db/schema.rb
+++ db/schema.rb
@@ -14,45 +14,48 @@
 ActiveRecord::Schema.define(version: 20150206134520) do
 
   create_table "aspects", force: :cascade do |t|
-    t.integer  "story_id"
-    t.string   "aspect"
-    t.string   "type"
-    t.integer  "order"
+    t.integer  "story_id",       limit: 4
+    t.string   "aspect",         limit: 255
+    t.string   "type",           limit: 255
+    t.integer  "order",          limit: 4
     t.string   "badge",          limit: 10
-    t.text     "text_content"
-    t.binary   "binary_content"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.text     "text_content",   limit: 65535
+    t.binary   "binary_content", limit: 65535
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
   end
 
-  add_index "aspects", ["story_id"], name: "index_aspects_on_story_id"
+  add_index "aspects", ["story_id"], name: "index_aspects_on_story_id", using: :btree
 
   create_table "stories", force: :cascade do |t|
-    t.string   "title"
-    t.text     "content"
-    t.string   "image_url"
-    t.string   "source_url"
+    t.string   "title",          limit: 255
+    t.text     "content",        limit: 65535
+    t.string   "image_url",      limit: 255
+    t.string   "source_url",     limit: 255
     t.date     "happened_on"
-    t.boolean  "popular_on_nos"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.boolean  "popular_on_nos", limit: 1
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
   end
 
   create_table "unlocks", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "aspect_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "user_id",    limit: 4
+    t.integer  "aspect_id",  limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
-  add_index "unlocks", ["aspect_id"], name: "index_unlocks_on_aspect_id"
-  add_index "unlocks", ["user_id"], name: "index_unlocks_on_user_id"
+  add_index "unlocks", ["aspect_id"], name: "index_unlocks_on_aspect_id", using: :btree
+  add_index "unlocks", ["user_id"], name: "index_unlocks_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name",       limit: 255
+    t.string   "password",   limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
+  add_foreign_key "aspects", "stories"
+  add_foreign_key "unlocks", "aspects"
+  add_foreign_key "unlocks", "users"
 end

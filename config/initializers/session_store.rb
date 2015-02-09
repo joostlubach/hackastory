80443c55f3ecaae18ba4d422f8a93d942b821356
Joost Lubach <joost@yoazt.com>
2015-02-09 09:30:34 +0100

    Prepare for new upload to Heroku

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_Hackastory_session'
+Rails.application.config.session_store :cookie_store, key: '_NewsBricks_session'
